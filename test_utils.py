38e02bc8d2338f9d31e265468cd2deeb176bfea1
Evgeniy Ratuev <evratal@yandex.ru>
2023-09-10 20:43:37 +0400

    Finish

--- test_utils.py
+++ test_utils.py
@@ -1,4 +1,13 @@
-from utills import load_file, filter_file, sort_file, get_last_operation
+from utills import load_file, filter_file, sort_file, get_last_operation, finish_file
+
+old_file = load_file()
+filt_file = filter_file(old_file)
+sor_file = sort_file(filt_file)
+fin_file = get_last_operation(sor_file, 5)
+redact_file = finish_file(fin_file)
+
+
+
 
 
 def test_load_file():
@@ -6,12 +15,15 @@ def test_load_file():
 
 
 def test_filter_file():
-    assert filter_file()[1]["state"] == "EXECUTED"
+    assert filter_file(old_file)[1]["state"] == "EXECUTED"
 
 
 def test_sort_file():
-    assert sort_file()[1]["date"][:10] == "2019-12-07"
+    assert sort_file(filt_file)[1]["date"][:10] == "2019-12-07"
 
 
 def test_get_last_operation():
-    assert get_last_operation(1)[0]["id"] == 863064926
+    assert get_last_operation(sor_file,1)[0]["id"] == 863064926
+
+def test_finish_file():
+    assert type(finish_file(fin_file)) == list
--- utills.py
+++ utills.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import json
 
 
@@ -7,35 +8,48 @@ def load_file():
         return old_file
 
 
-def filter_file():
+def filter_file(not_filt_file):
     filter_list = []
-    old_list = load_file()
-    for row in old_list:
+    for row in not_filt_file:
         if "state" in row and row['state'] == 'EXECUTED':
             filter_list.append(row)
     return filter_list
 
 
-def sort_file():
-    work_list = filter_file()
+def sort_file(not_sort_list):
+    work_list = not_sort_list
     work_list.sort(key=lambda d: d['date'], reverse=True)
     return work_list
 
 
-def get_last_operation(number_last_values):
-    operation_list = sort_file()
+def get_last_operation(file, number_last_values):
+    operation_list = file
     operation_list = operation_list[:number_last_values]
     return operation_list
 
 
-print(load_file())
-print('*************************')
-print(filter_file())
-for row in filter_file():
-    print (row['date'])
-print('----------------------')
-print(sort_file())
-for row in sort_file():
-    print (row['date'])
-print('...................')
-print(get_last_operation(1)[0])
+def finish_file(list_operation):
+    finish_list = []
+    for row in list_operation:
+        date = datetime.strptime(row["date"], "%Y-%m-%dT%H:%M:%S.%f").strftime("%d.%m.%Y")
+        description = row["description"]
+
+        if "from" in row:
+            string_from_letter = "".join(letter for letter in row["from"] if letter.isalpha() and not letter.isdigit())
+            string_from_digit = "".join(letter for letter in row["from"] if letter.isdigit() and not letter.isalpha())
+            new_string_from_digit = " " + string_from_digit[:4] + " " + string_from_digit[
+                                                                        4:6] + "** **** " + string_from_digit[-4:]
+            finish_string_from = string_from_letter + new_string_from_digit
+        else:
+            finish_string_from = ''
+        string_to = "Счет **" + row["to"][-4:]
+        string_bill = row["operationAmount"]["amount"] + " " + row["operationAmount"]["currency"]["name"]
+
+        finish_list.append(f"""\
+ {date} {description}
+ {finish_string_from}->{string_to}
+ {string_bill}""")
+
+    return finish_list
+
+
