04fbf7faf08b0ac83bf4ce58a619112eab532f74
Evgeniy Ratuev <evratal@yandex.ru>
2023-09-10 15:39:14 +0400

    Functions load and her test

--- test_utils.py
+++ test_utils.py
@@ -1,3 +1,5 @@
 from utills import load_file
+
+
 def test_load_file():
     assert type(load_file()) == list
--- utills.py
+++ utills.py
@@ -1,6 +1,8 @@
 import json
 
+
 def load_file():
-    with open ('operations.json', 'r', encoding='utf-8') as file:
+    with open('operations.json', 'r', encoding='utf-8') as file:
         old_file = json.load(file)
         return old_file
+
